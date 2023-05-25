c5bad1332d73dfc4ff9388fa1fa024503283a149
MarcusVinicius14 <marcus.vas14@gmail.com>
2023-05-25 20:18:35 -0300

    first changes

--- .pnp.cjs
+++ .pnp.cjs
@@ -33,6 +33,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@vime/core", "npm:5.4.0"],\
             ["@vime/react", "virtual:067e051375a2f7455bbf63a3345ff402fb4f1fad9e60bb08f7e30b6deac8b9996c320d75d161e89ce759a38e9752bb4e78f1ae9f98b606dc6bee428b9bb1a976#npm:5.4.0"],\
             ["@vitejs/plugin-react", "virtual:067e051375a2f7455bbf63a3345ff402fb4f1fad9e60bb08f7e30b6deac8b9996c320d75d161e89ce759a38e9752bb4e78f1ae9f98b606dc6bee428b9bb1a976#npm:4.0.0"],\
+            ["axios", "npm:1.4.0"],\
             ["eslint", "npm:8.39.0"],\
             ["eslint-plugin-react", "virtual:067e051375a2f7455bbf63a3345ff402fb4f1fad9e60bb08f7e30b6deac8b9996c320d75d161e89ce759a38e9752bb4e78f1ae9f98b606dc6bee428b9bb1a976#npm:7.32.2"],\
             ["eslint-plugin-react-hooks", "virtual:067e051375a2f7455bbf63a3345ff402fb4f1fad9e60bb08f7e30b6deac8b9996c320d75d161e89ce759a38e9752bb4e78f1ae9f98b606dc6bee428b9bb1a976#npm:4.6.0"],\
@@ -1172,11 +1173,32 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "linkType": "HARD"\
         }]\
       ]],\
+      ["asynckit", [\
+        ["npm:0.4.0", {\
+          "packageLocation": "./.yarn/cache/asynckit-npm-0.4.0-c718858525-7b78c451df.zip/node_modules/asynckit/",\
+          "packageDependencies": [\
+            ["asynckit", "npm:0.4.0"]\
+          ],\
+          "linkType": "HARD"\
+        }]\
+      ]],\
       ["available-typed-arrays", [\
         ["npm:1.0.5", {\
           "packageLocation": "./.yarn/cache/available-typed-arrays-npm-1.0.5-88f321e4d3-20eb47b3ce.zip/node_modules/available-typed-arrays/",\
           "packageDependencies": [\
             ["available-typed-arrays", "npm:1.0.5"]\
+          ],\
+          "linkType": "HARD"\
+        }]\
+      ]],\
+      ["axios", [\
+        ["npm:1.4.0", {\
+          "packageLocation": "./.yarn/cache/axios-npm-1.4.0-4d7ce8ca3e-7fb6a4313b.zip/node_modules/axios/",\
+          "packageDependencies": [\
+            ["axios", "npm:1.4.0"],\
+            ["follow-redirects", "virtual:4d7ce8ca3e1e44d82523fba2ad95e1be18c4e9f8dec6d551377587540da3ed75bd8bd3e812280309a3b90cfdb0560f076f3552a20839f7f15665207a4fbd588a#npm:1.15.2"],\
+            ["form-data", "npm:4.0.0"],\
+            ["proxy-from-env", "npm:1.1.0"]\
           ],\
           "linkType": "HARD"\
         }]\
@@ -1405,6 +1427,16 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "linkType": "HARD"\
         }]\
       ]],\
+      ["combined-stream", [\
+        ["npm:1.0.8", {\
+          "packageLocation": "./.yarn/cache/combined-stream-npm-1.0.8-dc14d4a63a-49fa4aeb49.zip/node_modules/combined-stream/",\
+          "packageDependencies": [\
+            ["combined-stream", "npm:1.0.8"],\
+            ["delayed-stream", "npm:1.0.0"]\
+          ],\
+          "linkType": "HARD"\
+        }]\
+      ]],\
       ["concat-map", [\
         ["npm:0.0.1", {\
           "packageLocation": "./.yarn/cache/concat-map-npm-0.0.1-85a921b7ee-902a9f5d89.zip/node_modules/concat-map/",\
@@ -1517,6 +1549,15 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "linkType": "HARD"\
         }]\
       ]],\
+      ["delayed-stream", [\
+        ["npm:1.0.0", {\
+          "packageLocation": "./.yarn/cache/delayed-stream-npm-1.0.0-c5a4c4cc02-46fe6e83e2.zip/node_modules/delayed-stream/",\
+          "packageDependencies": [\
+            ["delayed-stream", "npm:1.0.0"]\
+          ],\
+          "linkType": "HARD"\
+        }]\
+      ]],\
       ["delegates", [\
         ["npm:1.0.0", {\
           "packageLocation": "./.yarn/cache/delegates-npm-1.0.0-9b1942d75f-a51744d9b5.zip/node_modules/delegates/",\
@@ -1545,6 +1586,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@vime/core", "npm:5.4.0"],\
             ["@vime/react", "virtual:067e051375a2f7455bbf63a3345ff402fb4f1fad9e60bb08f7e30b6deac8b9996c320d75d161e89ce759a38e9752bb4e78f1ae9f98b606dc6bee428b9bb1a976#npm:5.4.0"],\
             ["@vitejs/plugin-react", "virtual:067e051375a2f7455bbf63a3345ff402fb4f1fad9e60bb08f7e30b6deac8b9996c320d75d161e89ce759a38e9752bb4e78f1ae9f98b606dc6bee428b9bb1a976#npm:4.0.0"],\
+            ["axios", "npm:1.4.0"],\
             ["eslint", "npm:8.39.0"],\
             ["eslint-plugin-react", "virtual:067e051375a2f7455bbf63a3345ff402fb4f1fad9e60bb08f7e30b6deac8b9996c320d75d161e89ce759a38e9752bb4e78f1ae9f98b606dc6bee428b9bb1a976#npm:7.32.2"],\
             ["eslint-plugin-react-hooks", "virtual:067e051375a2f7455bbf63a3345ff402fb4f1fad9e60bb08f7e30b6deac8b9996c320d75d161e89ce759a38e9752bb4e78f1ae9f98b606dc6bee428b9bb1a976#npm:4.6.0"],\
@@ -2035,12 +2077,46 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "linkType": "HARD"\
         }]\
       ]],\
+      ["follow-redirects", [\
+        ["npm:1.15.2", {\
+          "packageLocation": "./.yarn/cache/follow-redirects-npm-1.15.2-1ec1dd82be-faa66059b6.zip/node_modules/follow-redirects/",\
+          "packageDependencies": [\
+            ["follow-redirects", "npm:1.15.2"]\
+          ],\
+          "linkType": "SOFT"\
+        }],\
+        ["virtual:4d7ce8ca3e1e44d82523fba2ad95e1be18c4e9f8dec6d551377587540da3ed75bd8bd3e812280309a3b90cfdb0560f076f3552a20839f7f15665207a4fbd588a#npm:1.15.2", {\
+          "packageLocation": "./.yarn/__virtual__/follow-redirects-virtual-359bc4c55c/0/cache/follow-redirects-npm-1.15.2-1ec1dd82be-faa66059b6.zip/node_modules/follow-redirects/",\
+          "packageDependencies": [\
+            ["follow-redirects", "virtual:4d7ce8ca3e1e44d82523fba2ad95e1be18c4e9f8dec6d551377587540da3ed75bd8bd3e812280309a3b90cfdb0560f076f3552a20839f7f15665207a4fbd588a#npm:1.15.2"],\
+            ["@types/debug", null],\
+            ["debug", null]\
+          ],\
+          "packagePeers": [\
+            "@types/debug",\
+            "debug"\
+          ],\
+          "linkType": "HARD"\
+        }]\
+      ]],\
       ["for-each", [\
         ["npm:0.3.3", {\
           "packageLocation": "./.yarn/cache/for-each-npm-0.3.3-0010ca8cdd-6c48ff2bc6.zip/node_modules/for-each/",\
           "packageDependencies": [\
             ["for-each", "npm:0.3.3"],\
             ["is-callable", "npm:1.2.7"]\
+          ],\
+          "linkType": "HARD"\
+        }]\
+      ]],\
+      ["form-data", [\
+        ["npm:4.0.0", {\
+          "packageLocation": "./.yarn/cache/form-data-npm-4.0.0-916facec2d-01135bf867.zip/node_modules/form-data/",\
+          "packageDependencies": [\
+            ["form-data", "npm:4.0.0"],\
+            ["asynckit", "npm:0.4.0"],\
+            ["combined-stream", "npm:1.0.8"],\
+            ["mime-types", "npm:2.1.35"]\
           ],\
           "linkType": "HARD"\
         }]\
@@ -2862,6 +2938,25 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "linkType": "HARD"\
         }]\
       ]],\
+      ["mime-db", [\
+        ["npm:1.52.0", {\
+          "packageLocation": "./.yarn/cache/mime-db-npm-1.52.0-b5371d6fd2-0d99a03585.zip/node_modules/mime-db/",\
+          "packageDependencies": [\
+            ["mime-db", "npm:1.52.0"]\
+          ],\
+          "linkType": "HARD"\
+        }]\
+      ]],\
+      ["mime-types", [\
+        ["npm:2.1.35", {\
+          "packageLocation": "./.yarn/cache/mime-types-npm-2.1.35-dd9ea9f3e2-89a5b7f1de.zip/node_modules/mime-types/",\
+          "packageDependencies": [\
+            ["mime-types", "npm:2.1.35"],\
+            ["mime-db", "npm:1.52.0"]\
+          ],\
+          "linkType": "HARD"\
+        }]\
+      ]],\
       ["minimatch", [\
         ["npm:3.1.2", {\
           "packageLocation": "./.yarn/cache/minimatch-npm-3.1.2-9405269906-c154e56640.zip/node_modules/minimatch/",\
@@ -3350,6 +3445,15 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["loose-envify", "npm:1.4.0"],\
             ["object-assign", "npm:4.1.1"],\
             ["react-is", "npm:16.13.1"]\
+          ],\
+          "linkType": "HARD"\
+        }]\
+      ]],\
+      ["proxy-from-env", [\
+        ["npm:1.1.0", {\
+          "packageLocation": "./.yarn/cache/proxy-from-env-npm-1.1.0-c13d07f26b-ed7fcc2ba0.zip/node_modules/proxy-from-env/",\
+          "packageDependencies": [\
+            ["proxy-from-env", "npm:1.1.0"]\
           ],\
           "linkType": "HARD"\
         }]\
--- src/components/movies/index.jsx
+++ src/components/movies/index.jsx
@@ -2,8 +2,7 @@ import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import * as C from "./style";
 
-
-export const Movies = ({data, handleVideo}) => {
+export const Movies = ({ data, handleVideo }) => {
   const responsive = {
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
@@ -25,31 +24,35 @@ export const Movies = ({data, handleVideo}) => {
 
   return (
     <C.Container>
-      {
-      data?.categories?.map((category) => (
+      {data?.categories?.map((category) => (
         <>
-          <C.Text key={category?.type} id={category?.type}>{category?.name}</C.Text>
+          <C.Text key={category?.type} id={category?.type}>
+            {category?.name}
+          </C.Text>
           <Carousel responsive={responsive}>
-            {
-            getMovies(category?.type).map((data, index) => (
-              <C.Movie src={data?.imageSmall} alt={data?.title} key={index}
-              onClick={() => handleVideo(data)}
-              />
-            ))
-            }
-          </Carousel>
-        </>
-      ))
-      }
-
-<C.Text id="All">All</C.Text>
-          <Carousel responsive={responsive}>
-            {data?.movies.map((data, index) => (
-              <C.Movie src={data?.imageSmall} alt={data?.title} key={index} 
-              onClick={() => handleVideo(data)}
+            {getMovies(category?.type).map((data, index) => (
+              <C.Movie
+                src={data?.imageSmall}
+                alt={data?.title}
+                key={index}
+                onClick={() => handleVideo(data)}
               />
             ))}
           </Carousel>
+        </>
+      ))}
+
+      <C.Text id="All">All</C.Text>
+      <Carousel responsive={responsive}>
+        {data?.movies.map((data, index) => (
+          <C.Movie
+            src={data?.imageSmall}
+            alt={data?.title}
+            key={index}
+            onClick={() => handleVideo(data)}
+          />
+        ))}
+      </Carousel>
     </C.Container>
   );
 };
